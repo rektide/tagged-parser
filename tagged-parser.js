a4e89f431e9a9060105338190c94d23575b56b9a
rektide <rektide@voodoowarez.com>
2016-07-27 23:06:37 -0400

    Allow passing in of a tagged template function

--- tagged-parser.js
+++ tagged-parser.js
@@ -14,20 +14,42 @@ function parse(str){
 		index= loop.index + literal.length + expr.length + 3
 	}
 	literals.push( str.slice( index))
-	function execute( ctx, eval_){
+	function execute( ctx, taggedTemplater, eval_){
 		eval_= eval_|| eval
-		var ctxStrs= []
-		for( var i in ctx){
-			ctxStrs.push( i, "=", JSON.stringify( ctx[ i]), ";")
+		var templater= !!taggedTemplater|| taggedTemplater=== false
+
+		// serialize context
+		if( typeof ctx=== "object"){
+			var ctxStrs= []
+			for( var i in ctx){
+				ctxStrs.push( i, "=", JSON.stringify( ctx[ i]), ";")
+			}
+			ctx= ctxStrs.join( "")
 		}
-		var ctxStr= ctxStrs.join( "")
-		var expr= []
-		for( var i= 0; i< execute.exprs.length; ++i){
-			var val= eval_( ctxStr + execute.exprs[ i])
-			expr.push( execute.literals[i], val)
+		ctx= ctx || ""
+
+		// evaluate all expressions
+		var expr= templater ? [execute.literals] : []
+		for(var i= 0; i< execute.exprs.length; ++i){
+			var val= eval_( ctx+ execute.exprs[ i])
+			if( templater){
+				expr.push(val) // gather expr
+			}else{
+				expr.push( execute.literals[i], val) // concat in pieces
+			}
 		}
-		expr.push( execute.literals[ i]|| "")
-		return expr.join( "")
+
+		if( taggedTemplater){
+			// run tagged templater
+			return taggedTemplater.apply(null, expr)
+		}else if( taggedTemplater=== false){
+			// pass back raw components to apply to a tagged templater
+			return expr
+		}else{
+			// execute a condensed equivalent to String.raw
+			expr.push( execute.literals[ i]|| "")
+			return expr.join( "")
+		}
 	}
 	execute.literals= literals
 	execute.exprs= exprs
